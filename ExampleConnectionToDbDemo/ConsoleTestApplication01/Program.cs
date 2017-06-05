310389598dc1a6382810e8f247d6392801c76617
theshaftman <mariyan_apostolov@mail.bg>
2017-06-05 23:51:48 +0300

    SQLite create database tables

--- ExampleConnectionToDbDemo/ConsoleTestApplication01/Program.cs
+++ ExampleConnectionToDbDemo/ConsoleTestApplication01/Program.cs
@@ -13,23 +13,23 @@
                 Console.WriteLine($"{item.Id} {item.Username} {item.Password} {item.Description} {item.UserTypeId}");
             }
 
-           // Console.WriteLine("---");
-           // // SQLite connection test
-           // TestSqLiteConnection.CreateFile();
-           // TestSqLiteConnection.CreateUsers();
-           // var sqLite = TestSqLiteConnection.GetUsers();
-           // foreach (var item in sqLite)
-           // {
-           //     Console.WriteLine($"{item.Id} {item.Name}");
-           // }
-           //
-           // Console.WriteLine("---");
-           // // SQL Server connection test
-           // var sqlServer = TestSQLServerConnection.GetServices();
-           // foreach (var item in sqlServer)
-           // {
-           //     Console.WriteLine($"{item.Id} {item.Name}");
-           // }
+            // Console.WriteLine("---");
+            // SQLite connection test
+            TestSqLiteConnection.CreateFile();
+            TestSqLiteConnection.CreateDb();
+            //var sqLite = TestSqLiteConnection.GetUsers();
+            //foreach (var item in sqLite)
+            //{
+            //    Console.WriteLine($"{item.Id} {item.Name}");
+            //}
+
+            Console.WriteLine("---");
+            // SQL Server connection test
+            var sqlServer = TestSQLServerConnection.GetServices();
+            foreach (var item in sqlServer)
+            {
+                Console.WriteLine($"{item.Id} {item.Name}");
+            }
         }
     }
 }
--- ExampleConnectionToDbDemo/ConsoleTestApplication01/TestSqLiteConnection.cs
+++ ExampleConnectionToDbDemo/ConsoleTestApplication01/TestSqLiteConnection.cs
@@ -1,60 +1,102 @@
-﻿//namespace ConsoleTestApplication01
-//{
-//    using System;
-//    using System.Collections.Generic;
-//    using System.Data.SQLite;
-//
-//    internal class TestSqLiteConnection
-//    {
-//        private static string fileName = "sample.db3";
-//        private static SQLiteConnection conn = new SQLiteConnection(String.Format($"data source={fileName}"));
-//        private static SQLiteCommand cmd = new SQLiteCommand(conn);
-//
-//        internal static bool CreateFile()
-//        {
-//            SQLiteConnection.CreateFile(fileName);
-//            return true;
-//        }
-//
-//        internal static bool CreateUsers()
-//        {
-//            try
-//            {
-//                string createQuery = @"CREATE TABLE IF NOT EXISTS
-//                                [Users] (
-//                                    [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-//                                    [Name] NVARCHAR(256) NULL
-//                                )";
-//                conn.Open();
-//                cmd.CommandText = createQuery;
-//                cmd.ExecuteNonQuery();
-//                cmd.CommandText = "INSERT INTO Users (Name) VALUES('Alex')";
-//                cmd.ExecuteNonQuery();
-//                conn.Close();
-//
-//                return true;
-//            }
-//            catch (Exception)
-//            {
-//                return false;
-//            }
-//        }
-//
-//        internal static IList<User> GetUsers()
-//        {
-//            IList<User> list = new List<User>();
-//            conn.Open();
-//            cmd.CommandText = "SELECT * FROM Users";
-//            using (SQLiteDataReader reader = cmd.ExecuteReader())
-//            {
-//                while (reader.Read())
-//                {
-//                    list.Add(new User(int.Parse(reader["Id"].ToString()), reader["Name"].ToString()));
-//                }
-//            }
-//
-//            conn.Close();
-//            return list;
-//        }
-//    }
-//}
+﻿namespace ConsoleTestApplication01
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data.SQLite;
+
+    internal class TestSqLiteConnection
+    {
+        private static string fileName = "sample.db";
+        private static SQLiteConnection conn = new SQLiteConnection(String.Format($"data source={fileName}"));
+        private static SQLiteCommand cmd = new SQLiteCommand(conn);
+
+        internal static bool CreateFile()
+        {
+            SQLiteConnection.CreateFile(fileName);
+            return true;
+        }
+
+        internal static bool CreateDb()
+        {
+            string createQuery = @"PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS [CarCategories] (
+       [CategoryId] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+       [CategoryName] NVARCHAR(32) NULL
+);
+CREATE UNIQUE INDEX Unq_Category ON [CarCategories]([CategoryName]);
+
+CREATE TABLE IF NOT EXISTS [CarBrands] (
+       [BrandId] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+       [BrandName] NVARCHAR(32) NULL
+);	
+CREATE UNIQUE INDEX Unq_Brand ON [CarBrands]([BrandName]);
+
+CREATE TABLE IF NOT EXISTS [CarModels] (
+       [ModelId] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+       [ModelName] NVARCHAR(32) NULL
+);		
+CREATE UNIQUE INDEX Unq_Model ON [CarModels]([ModelName]);
+
+CREATE TABLE IF NOT EXISTS [Cars] (
+       [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	   [CategoryId] INTEGER NOT NULL,
+	   [BrandId] INTEGER NOT NULL,
+	   [ModelId] INTEGER NOT NULL,
+	   [Details] TEXT NULL,
+       [IsTaken] INTEGER DEFAULT 0 CHECK([IsTaken] IN (0,1)),
+	   FOREIGN KEY ([CategoryId]) REFERENCES [CarCategories] ([CategoryId]),
+	   FOREIGN KEY ([BrandId]) REFERENCES [CarBrands] ([BrandId]),
+	   FOREIGN KEY ([ModelId]) REFERENCES [CarModels] ([ModelId])
+);";
+
+            conn.Open();
+            cmd.CommandText = createQuery;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+
+            return true;
+        }
+
+        internal static bool CreateUsers()
+        {
+            try
+            {
+                string createQuery = @"CREATE TABLE IF NOT EXISTS
+                                [Users] (
+                                    [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                    [Name] NVARCHAR(256) NULL
+                                )";
+                conn.Open();
+                cmd.CommandText = createQuery;
+                cmd.ExecuteNonQuery();
+                cmd.CommandText = "INSERT INTO Users (Name) VALUES('Alex')";
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        internal static IList<User> GetUsers()
+        {
+            IList<User> list = new List<User>();
+            conn.Open();
+            cmd.CommandText = "SELECT * FROM [Users]";
+            using (SQLiteDataReader reader = cmd.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    list.Add(new User(int.Parse(reader["Id"].ToString()), reader["Name"].ToString()));
+                }
+            }
+
+            conn.Close();
+            return list;
+        }
+    }
+}
