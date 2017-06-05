4816f88723d4319aa772d8b606c72c8904f30269
Exodary <krasimir_zlatkov@mail.bg>
2017-06-05 23:00:43 +0300

    Added Connection To PostgreSQL

--- ExampleConnectionToDbDemo/ConsoleTestApplication01/Program.cs
+++ ExampleConnectionToDbDemo/ConsoleTestApplication01/Program.cs
@@ -7,29 +7,29 @@
         public static void Main(string[] args)
         {
             // PostgreSQL connection test
-            var postgreSql = TestPostgreSQLConnection.GetCars();
+            var postgreSql = TestPostgreSQLConnection.GetUsers();
             foreach (var item in postgreSql)
             {
-                Console.WriteLine($"{item.Id} {item.Name}");
+                Console.WriteLine($"{item.Id} {item.Username} {item.Password} {item.Description} {item.UserTypeId}");
             }
 
-            Console.WriteLine("---");
-            // SQLite connection test
-            TestSqLiteConnection.CreateFile();
-            TestSqLiteConnection.CreateUsers();
-            var sqLite = TestSqLiteConnection.GetUsers();
-            foreach (var item in sqLite)
-            {
-                Console.WriteLine($"{item.Id} {item.Name}");
-            }
-
-            Console.WriteLine("---");
-            // SQL Server connection test
-            var sqlServer = TestSQLServerConnection.GetServices();
-            foreach (var item in sqlServer)
-            {
-                Console.WriteLine($"{item.Id} {item.Name}");
-            }
+           // Console.WriteLine("---");
+           // // SQLite connection test
+           // TestSqLiteConnection.CreateFile();
+           // TestSqLiteConnection.CreateUsers();
+           // var sqLite = TestSqLiteConnection.GetUsers();
+           // foreach (var item in sqLite)
+           // {
+           //     Console.WriteLine($"{item.Id} {item.Name}");
+           // }
+           //
+           // Console.WriteLine("---");
+           // // SQL Server connection test
+           // var sqlServer = TestSQLServerConnection.GetServices();
+           // foreach (var item in sqlServer)
+           // {
+           //     Console.WriteLine($"{item.Id} {item.Name}");
+           // }
         }
     }
 }
