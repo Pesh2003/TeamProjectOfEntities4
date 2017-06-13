6e9b68b2194980d4ad08b1b9608f84d286dd3e1b
theshaftman <mariyan_apostolov@mail.bg>
2017-06-13 04:08:09 +0300

    SQLite framework -> updated

--- CourierDistributionProject/CourierService.DbContext/SQLite/SqliteDbContext.cs
+++ CourierDistributionProject/CourierService.DbContext/SQLite/SqliteDbContext.cs
@@ -16,7 +16,13 @@ namespace CourierService.DbContext.SQLite
         private SQLiteConnection conn;
         private SQLiteCommand cmd;
         private SQLiteHashSet<Category> categories;
-        private ICollection<Category> categoriesToAdd;
+        private SQLiteHashSet<Category> categoriesToAdd;
+        private SQLiteHashSet<Brand> brands;
+        private SQLiteHashSet<Brand> brandsToAdd;
+        private SQLiteHashSet<DbContextModels.SQLite.Model> models;
+        private SQLiteHashSet<DbContextModels.SQLite.Model> modelsToAdd;
+        private SQLiteHashSet<Vehicle> vehicles;
+        private SQLiteHashSet<Vehicle> vehiclesToAdd;
 
         public SqliteDbContext(string fileName = "Vehicles.db")
         {
@@ -24,6 +30,13 @@ namespace CourierService.DbContext.SQLite
             this.conn = new SQLiteConnection(String.Format($"data source={this.fileName}"));
             this.categories = new SQLiteHashSet<Category>();
             this.categoriesToAdd = new SQLiteHashSet<Category>();
+            this.brands = new SQLiteHashSet<Brand>();
+            this.brandsToAdd = new SQLiteHashSet<Brand>();
+            this.models = new SQLiteHashSet<DbContextModels.SQLite.Model>();
+            this.modelsToAdd = new SQLiteHashSet<DbContextModels.SQLite.Model>();
+            this.vehicles = new SQLiteHashSet<Vehicle>();
+            this.vehiclesToAdd = new SQLiteHashSet<Vehicle>();
+
             this.CreateDbFile();
         }
 
@@ -37,7 +50,7 @@ namespace CourierService.DbContext.SQLite
                 {
                     this.categories.Add(new Category() { CategoryName = item["CategoryName"].ToString() });
                 }
-                this.categories.OnAdd += new EventHandler(l_OnAdd);
+                this.categories.OnAdd += new EventHandler(Hash_OnAdd);
                 return this.categories;
             }
             private set
@@ -45,12 +58,75 @@ namespace CourierService.DbContext.SQLite
                 this.categories = value;
             }
         }
-
-        private void l_OnAdd(object sender, EventArgs e)
+        
+        public SQLiteHashSet<Brand> Brands
         {
-            Console.WriteLine("Element added...");
+            get
+            {
+                this.brands.Clear();
+                ICollection<SQLiteDataReader> reader = this.GetReaderCollection("Brands");
+                foreach (var item in reader)
+                {
+                    this.brands.Add(new Brand()
+                    {
+                        BrandName = item["BrandName"].ToString(),
+                        CategoryId = int.Parse(item["CategoryId"].ToString())
+                    });
+                }
+                return this.brands;
+            }
+            private set
+            {
+                this.brands = value;
+            }
         }
 
+        public SQLiteHashSet<DbContextModels.SQLite.Model> Models
+        {
+            get
+            {
+                this.models.Clear();
+                ICollection<SQLiteDataReader> reader = this.GetReaderCollection("Models");
+                foreach (var item in reader)
+                {
+                    this.models.Add(new DbContextModels.SQLite.Model()
+                    {
+                        ModelName = item["ModelName"].ToString(),
+                        BrandId = int.Parse(item["BrandId"].ToString())
+                    });
+                }
+                return this.models;
+            }
+            private set
+            {
+                this.models = value;
+            }
+        }
+
+        public SQLiteHashSet<Vehicle> Vehicles
+        {
+            get
+            {
+                this.vehicles.Clear();
+                ICollection<SQLiteDataReader> reader = this.GetReaderCollection("Vehicles");
+                foreach (var item in reader)
+                {
+                    this.vehicles.Add(new Vehicle()
+                    {
+                        CategoryId = int.Parse(item["CategoryId"].ToString()),
+                        BrandId = int.Parse(item["BrandId"].ToString()),
+                        ModelId = int.Parse(item["ModelId"].ToString()),
+                        IsTaken = int.Parse(item["IsTaken"].ToString())
+                    });
+                }
+                return this.vehicles;
+            }
+            private set
+            {
+                this.vehicles = value;
+            }
+        }
+        
         public void SaveChanges()
         {
         }
@@ -58,6 +134,35 @@ namespace CourierService.DbContext.SQLite
         public void AddCategory(Category category)
         {
             this.InsertStatement("Categories", new string[1] { "CategoryName" }, new string[1] { category.CategoryName });
+        }
+
+        public void AddBrand(Brand brand)
+        {
+            this.InsertStatement(
+                "Brands", 
+                new string[2] { "BrandName", "CategoryId" }, 
+                new dynamic[2] { brand.BrandName, brand.CategoryId });
+        }
+
+        public void AddModel(DbContextModels.SQLite.Model model)
+        {
+            this.InsertStatement(
+                "Models", 
+                new string[2] { "ModelName", "BrandId" }, 
+                new dynamic[2] { model.ModelName, model.BrandId });
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            this.InsertStatement(
+                "Vehicles", 
+                new string[4] { "CategoryId", "BrandId", "ModelId", "IsTaken" }, 
+                new dynamic[4] { vehicle.CategoryId, vehicle.BrandId, vehicle.ModelId, 0 });
+        }
+
+        // Add sent object to added list to use the "SaveChanges" method correctly
+        private void Hash_OnAdd(object sender, EventArgs e)
+        {
         }
 
         private void CreateDbFile()
@@ -100,11 +205,13 @@ namespace CourierService.DbContext.SQLite
             return readerCollection;
         }
 
-        private void InsertStatement(string tableName, string[] properties, string[] values)
+        private void InsertStatement(string tableName, string[] properties, dynamic[] values)
         {
             using (this.cmd = new SQLiteCommand())
             {
                 this.conn.Open();
+
+                // TODO: Check variable type to include '' when having a varchar value
                 this.cmd.CommandText = string.Format($"INSERT INTO {tableName} ({string.Join(",", properties)}) VALUES('{string.Join(",", values)}');");
                 this.cmd.ExecuteNonQuery();
                 this.conn.Close();
